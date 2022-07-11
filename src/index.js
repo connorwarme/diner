e1369ba148a32f7cf760b6fe4f2d9b9f97bdbf9b
connorwarme <connor.warme@gmail.com>
2022-07-11 14:56:59 -0600

    added stylesheet, js for simple header

--- src/index.js
+++ src/index.js
@@ -8,8 +8,24 @@ const header = () => {
     const title = document.createElement('h1');
     title.classList.add('title');
     title.textContent = "Dirtbag Diner";
+    const menuContainer = document.createElement('div');
+    menuContainer.classList.add('menuContainer');
+    const home = document.createElement('div');
+    home.classList.add('home');
+    home.textContent = "HOME";
+    const menu = document.createElement('div');
+    menu.classList.add('menu');
+    menu.textContent = "MENU";
+    const contact = document.createElement('div');
+    contact.classList.add('contact');
+    contact.textContent = "CONTACT US";
     headerContainer.appendChild(titleContainer);
     titleContainer.appendChild(title);
+    headerContainer.appendChild(menuContainer);
+    menuContainer.appendChild(home);
+    menuContainer.appendChild(menu);
+    menuContainer.appendChild(contact);
     return headerContainer;
 }
-document.body.appendChild(header());
+const content = document.querySelector('div#content');
+content.appendChild(header());
