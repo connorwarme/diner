7c7ad966839e7ad7e5f8d69e0920ca5820031b58
connorwarme <connor.warme@gmail.com>
2022-07-11 14:09:43 -0600

    getting started with webpack and importing a js module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 console.log('fire in the hole');
+
+const header = () => {
+    const headerContainer = document.createElement('div');
+    headerContainer.classList.add('headerContainer');
+    const titleContainer = document.createElement('div');
+    titleContainer.classList.add('titleContainer');
+    const title = document.createElement('h1');
+    title.classList.add('title');
+    title.textContent = "Dirtbag Diner";
+    headerContainer.appendChild(titleContainer);
+    titleContainer.appendChild(title);
+    return headerContainer;
+}
+document.body.appendChild(header());
